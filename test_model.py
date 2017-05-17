dca651cb2e9f28df2f9a92bf719dfc23dc45864b
jsn5 <jaisonsaji@gmail.com>
2017-05-17 21:03:13 +0530

    using mss for screen

--- test_model.py
+++ test_model.py
@@ -5,12 +5,14 @@ from PIL import Image
 from resizeimage import resizeimage
 from alexnet import alexnet
 import pyxhook
+from mss import mss
+
 
 WIDTH = 160
 HEIGHT = 120
 LR = 1e-3
 EPOCHS = 10
-MODEL_NAME = 'tuxcart.model'
+MODEL_NAME = 'output/tuxcart.model'
 model = alexnet(WIDTH, HEIGHT, LR)
 model.load(MODEL_NAME)
 
@@ -37,6 +39,13 @@ new_hook.HookKeyboard()
 new_hook.start()
 
 
+def getImage():
+	with mss() as sct:
+		# The screen part to capture
+		mon = {'top': 65, 'left': 50, 'width': 1020, 'height': 720}
+		sct.get_pixels(mon)
+		img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
+	return img
 
 def straight():
 	pyautogui.keyDown('w')
@@ -49,14 +58,12 @@ def straight():
 
 def left():
 	pyautogui.keyDown('a')
-	pyautogui.keyDown('w')
 	pyautogui.keyUp('d')
 	pyautogui.keyUp('s')
 	pyautogui.keyUp('a')
 
 def right():
 	pyautogui.keyDown('d')
-	pyautogui.keyDown('w')
 	pyautogui.keyUp('a')
 	pyautogui.keyUp('s')
 	pyautogui.keyUp('d')
@@ -69,9 +76,6 @@ def back():
 
 def nokey():
 	pyautogui.keyDown('w')
-	pyautogui.keyUp('a')
-	pyautogui.keyUp('s')
-	pyautogui.keyUp('d')
 
 
 
@@ -80,34 +84,36 @@ def main():
 		print(i+1)
 		time.sleep(1)
 
+
 	count=0
 	while(True):
-
 		if not paused:
-
 			# 800x600 windowed mode
-			screen = pyautogui.screenshot(region=(65,50, 800, 600))
+			avg_prediction = [0,0,0]
+			screen = getImage()
 			last_time = time.time()
 			screen = screen.convert('L')
 			screen = resizeimage.resize_cover(screen, [160, 120], validate=False)
 			screen = np.array(screen)
-			prediction = model.predict([screen.reshape(160,120,1)])[0]
+			prediction = model.predict([screen.reshape(160,120,1)])[0]		
+			fwd_threshold=0.9
+			turn_threshold=0.5
 			print(prediction)
-			turn_thresh = 0.6
-			fwd_thresh = 0.6
-
-			max_ = max(prediction)
-			if prediction[0] > turn_thresh:
+			max_val = max(prediction)
+			if prediction[0] >= turn_threshold:
 				print("right")
 				right()
-			elif prediction[1] > turn_thresh:
+			elif prediction[1] >= turn_threshold:
 				left()
 				print("left")
-			elif prediction[2] == max_:
+			elif prediction[2] >= fwd_threshold:
 				straight()
 				print("straight")
 			else:
-				nokey()
+				back()
+			nokey()
+			
+			print("fps="+str(time.time()-last_time))
 			
 				
 
